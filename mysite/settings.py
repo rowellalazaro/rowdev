4ef426bc18cdbfc510ae8256b88496a663d04599
Rowella Lazaro <rowellaxlazaro@gmail.com>
2026-06-02 09:45:32 +0800

    add email settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,10 @@ SOCIALACCOUNT_PROVIDERS = {
         'AUTH_PARAMS': {'access_type': 'online'},
     }
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'roxlibao@gmail.com'
+EMAIL_HOST_PASSWORD = 'wxlf xdyn fkdl kphl'
+DEFAULT_FROM_EMAIL = 'roxlibao@gmail.com'
