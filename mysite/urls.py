e187ac6e0adc761bb297b485771de93bbc59c5c5
Rowella Lazaro <rowellaxlazaro@gmail.com>
2026-06-02 09:51:34 +0800

    forgot pass

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import (
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +32,11 @@ urlpatterns = [
     path('', include('myapp.urls')),
 
     path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('password-reset/', PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
